a88861726558251286dd97549b97fdc697b35575
Rubendiazolmo <86986250+Rubendiazolmo@users.noreply.github.com>
2022-04-30 12:51:57 +0200

    Create backup.py

---
+++ backup.py
@@ -0,0 +1,34 @@
+from os import listdir, getcwd, system, getlogin
+from os.path import isfile, join, getmtime, splitext
+from datetime import datetime
+
+user = (getlogin())
+user = "RDO"
+
+extensions = [".7z",
+              ".zap13",
+              ".zap14",
+              ".zap15",
+              ",zap15_1",
+              ".zap16",
+              ".zip",
+              ".rar"]
+
+cwd = getcwd()
+
+if not "OLD" in listdir(cwd):
+    system("mkdir OLD")
+
+files = [f for f in listdir(cwd) if isfile(join(cwd, f))]
+
+for file in files:
+    
+    name, extension = (splitext(file))
+
+    if extension in extensions:
+        
+        date = (getmtime(file))
+        date = datetime.utcfromtimestamp(date).strftime('%Y%m%d_%H%M')
+        new_file = 'OLD\\'+name + "_" + date + extension
+        cmd = " ".join(('move', join(cwd,file), join(cwd,new_file)))
+        system(cmd)

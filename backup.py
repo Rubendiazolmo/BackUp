588e49b6192e5e32f58c2e590e3b3a9c91849887
Rubendiazolmo <86986250+Rubendiazolmo@users.noreply.github.com>
2022-04-30 17:12:15 +0200

    Update backup.py

--- backup.py
+++ backup.py
@@ -2,7 +2,24 @@ from os import listdir, getcwd, system, getlogin
 from os.path import isfile, join, getmtime, splitext
 from datetime import datetime
 
+# Función para comprobar si el nombre del archivo ya tiene la fecha incluida
+def TieneFecha(str):
+
+    str = str[-14::]
+    if str[0] == "_" and str[9] == "_":
+
+        try:
+            _ = int(str[1:8])
+            _ = int(str[10:14])
+            return True
+            
+        except ValueError:
+            return False
+    else:
+        return False
+
 user = (getlogin())
+user = "RDO"
 
 extensions = [".7z",
               ".zap13",
@@ -20,14 +37,19 @@ if not "OLD" in listdir(cwd):
 
 files = [f for f in listdir(cwd) if isfile(join(cwd, f))]
 
+
 for file in files:
     
     name, extension = (splitext(file))
 
     if extension in extensions:
         
-        date = (getmtime(file))
-        date = datetime.utcfromtimestamp(date).strftime('%Y%m%d_%H%M')
-        new_file = 'OLD\\'+name + "_" + date + extension
+        if not TieneFecha(name):
+            date = (getmtime(file))
+            date = datetime.utcfromtimestamp(date).strftime('%Y%m%d_%H%M')
+            new_file = 'OLD\\'+name + "_" + date + extension
+        else:
+            new_file = 'OLD\\'+name + extension
+            
         cmd = " ".join(('move', join(cwd,file), join(cwd,new_file)))
         system(cmd)
